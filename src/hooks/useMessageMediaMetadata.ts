0d3e1500a2dd21400d2c7dd5ebd29b5741c79239
yotsersaw <yotsersaw@gmail.com>
2026-02-20 10:27:26 +0000

    Fix QR code logo

--- src/hooks/useMessageMediaMetadata.ts
+++ src/hooks/useMessageMediaMetadata.ts
@@ -16,7 +16,7 @@ import useAsync from './useAsync';
 import useMedia from './useMedia';
 import useOldLang from './useOldLang';
 
-import telegramLogoPath from '../assets/telegram-logo-filled.svg';
+const telegramLogoPath = '/logo.png';
 
 const LOGO_DIMENSIONS = { width: 200, height: 200 };
 const MINIMAL_SIZE = 115; // spec says 100, but on Chrome 93 it's not showing
