481d23025152ea17e6ca3995392d1e3e248521ae
yotsersaw <yotsersaw@gmail.com>
2026-02-20 20:51:35 +0000

    Hardcode WarpGram title in AuthPhoneNumber

--- src/components/auth/AuthPhoneNumber.tsx
+++ src/components/auth/AuthPhoneNumber.tsx
@@ -254,7 +254,7 @@ const AuthPhoneNumber = ({
       )}
       <div className="auth-form">
         <div id="logo" />
-        <h1>{lang('AuthTitle')}</h1>
+        <h1>WarpGram</h1>
         <p className="note">{lang('StartText')}</p>
         <form className="form" action="" onSubmit={handleSubmit}>
           <CountryCodeInput
