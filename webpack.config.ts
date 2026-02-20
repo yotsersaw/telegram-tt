a00cf47ee28f8b9d6fa274600e99af0a743d928c
yotsersaw <yotsersaw@gmail.com>
2026-02-20 16:07:26 +0000

    Set app title to WarpGram in webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -28,7 +28,7 @@ const {
   HTTPS_KEY_PATH = '',
 } = process.env;
 
-const DEFAULT_APP_TITLE = `Telegram${APP_ENV !== 'production' ? ' Beta' : ''}`;
+const DEFAULT_APP_TITLE = 'WarpGram';
 
 // GitHub workflow uses an empty string as the default value if it's not in repository variables, so we cannot define a default value here
 process.env.BASE_URL = process.env.BASE_URL || PRODUCTION_URL;
